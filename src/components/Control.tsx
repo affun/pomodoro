9eae9ff74b46017e472f058149e7a14a8c500d17
affangv8 <affangv8@gmail.com>
2024-05-17 17:20:11 +0000

    feat: made function to change seconds to min and sec format

--- src/components/Control.tsx
+++ src/components/Control.tsx
@@ -6,32 +6,32 @@ interface Params {
 
 const Control = (control: Params) => {
   const style = {
-    btn: "text-white uppercase text-xl py-1 px-4 rounded-md transition-all duration-300 ease-in-out",
+    btn: "uppercase text-xl py-1 px-4 rounded-md transition-all duration-300 ease-in-out",
   };
   return (
     <div className="flex flex-col items-center justify-center">
       <button
         onClick={control.start}
-        class={`${style.btn} bg-white text-red-500 hover:bg-gray-100`}
+        class={`${style.btn} bg-white hover:bg-gray-100 text-red-500`}
       >
         Start
       </button>
       <div className={`flex justify-between`}>
         <button
           onClick={control.reset}
-          class={`${style.btn} border-2 border-white`}
+          class={`${style.btn} border-2 border-white text-white`}
         >
           reset
         </button>
         <button
           onClick={control.pause}
-          class={`${style.btn} bg-yellow-500 hover:bg-yellow-400`}
+          class={`${style.btn} bg-yellow-500 hover:bg-yellow-400 text-white`}
         >
           Pause
         </button>
         <button
           onClick={control.start}
-          class={`${style.btn} bg-green-500 hover:bg-green-400`}
+          class={`${style.btn} bg-green-500 hover:bg-green-400 text-white`}
         >
           Resume
         </button>
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,13 @@
-const Timer = ({ time }: { time: Number }) => {
-  return <div>{time}</div>;
+const Timer = ({ time }: { time: number }) => {
+  const toMinutesAndSeconds = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60)
+      .toString()
+      .padStart(2, "0");
+    const remainingSeconds = (seconds % 60).toString().padStart(2, "0");
+    return `${minutes}:${remainingSeconds}`;
+  };
+
+  return <div>{toMinutesAndSeconds(time)}</div>;
 };
 
 export default Timer;
